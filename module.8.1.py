b6a3669542b0bac2ad12f0ef259708f7aadabc30
Daniil_Tchetyra <defender.0599@gmail.com>
2024-11-03 20:32:16 +0300

    Update module.8.1.py

--- module.8.1.py
+++ module.8.1.py
@@ -6,8 +6,6 @@ def add_everything_up(a, b):
     else:
         # Если типы разные (число и строка)
         return str(a) + str(b)
-
-# Примеры использования
 print(add_everything_up(123.456, 'строка'))  # Вывод: 123.456строка
 print(add_everything_up('яблоко', 4215))     # Вывод: яблоко4215
 print(add_everything_up(123.456, 7))          # Вывод: 130.456
